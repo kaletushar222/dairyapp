119035b1fe77fc7e4ffbf44dd0f9a52f862454a7
tushar kale <kaletushar222@gmail.com>
2023-02-08 01:46:22 +0530

    portfolio removed

--- src/layouts/Appmodules/Dairy/Components/Footer/Footer.js
+++ src/layouts/Appmodules/Dairy/Components/Footer/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
 
     render() {
 		return <center> 
-                    <b>Designed by </b>| <img height="20" width="20" src={web} alt="portfolio" /> <a target="_blank" href="https://kaletushar222.github.io/portfolio">Portfolio</a> | <img height="20" width="20" src={insta} alt="instagram" /> <a target="_blank" href="https://www.instagram.com/tu__shark/">Tushar Kale</a>
+                    <b>Designed by </b> | <img height="20" width="20" src={insta} alt="instagram" /> <a target="_blank" href="https://www.instagram.com/tu__shark/">Tushar Kale</a>
                 </center>
 	}
 }
