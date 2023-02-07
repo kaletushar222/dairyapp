e04b2ba7b62a5548d622cbaee400ddb297db3f80
tushar kale <kaletushar222@gmail.com>
2023-02-08 01:39:41 +0530

    instagram link updated

--- src/layouts/Appmodules/Dairy/Components/Footer/Footer.js
+++ src/layouts/Appmodules/Dairy/Components/Footer/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
 
     render() {
 		return <center> 
-                    <b>Designed by </b>| <img height="20" width="20" src={web} alt="portfolio" /> <a target="_blank" href="https://kaletushar222.github.io/portfolio">Tushar Kale</a> | <img height="20" width="20" src={insta} alt="instagram" /> <a target="_blank" href="https://www.instagram.com/t.u.s.h.a.r_k/">t.u.s.h.a.r_k</a>
+                    <b>Designed by </b>| <img height="20" width="20" src={web} alt="portfolio" /> <a target="_blank" href="https://kaletushar222.github.io/portfolio">Portfolio/a> | <img height="20" width="20" src={insta} alt="instagram" /> <a target="_blank" href="https://www.instagram.com/tu__shark/">Tushar Kale</a>
                 </center>
 	}
 }
