6997b6e41c4f07034d377f4d103256de4edc10c6
tushar kale <kaletushar222@gmail.com>
2025-05-12 02:01:07 +0530

    added website in footer and domain name changes

--- src/layouts/Appmodules/Dairy/Components/Footer/Footer.js
+++ src/layouts/Appmodules/Dairy/Components/Footer/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import insta from './insta.png';
+import web from './web.png';
 
 class Footer extends React.Component {
 
     render() {
 		return <center> 
-                    <b>Designed by </b> | <img height="20" width="20" src={insta} alt="instagram" /> <a target="_blank" href="https://www.instagram.com/tu__shark/">Tushar Kale</a>
+                    <b>Designed by </b> | <img height="20" width="20" src={web} alt="website" /> <a target="_blank" href="https://www.tushark.in">tushark.in</a>  | <img height="20" width="20" src={insta} alt="instagram" /> <a target="_blank" href="https://www.instagram.com/tu__shark/">Tushar Kale</a>
                 </center>
 	}
 }
